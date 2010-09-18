1091a9147a1e991945f45202b56b73e8f5b554db
ivo <ivo@danihelka.net>
2010-09-18 20:07:20 +0200

    Dragged the points only when clicking on them.

--- src/game/BoardState.hx
+++ src/game/BoardState.hx
@@ -13,6 +13,7 @@ import flash.ui.Keyboard;
  * A game state with some points on a board.
  */
 class BoardState implements GameState {
+    var _dotRadius : Float;
     var _dot : Blob;
     var _points : List<Point>;
     var _dragged : Point;
@@ -22,14 +23,21 @@ class BoardState implements GameState {
         _points.push(new Point(80, 60));
         _points.push(new Point(120, 80));
         _points.push(new Point(200, 100));
-        _dot = new Blob(new game.assets.Dot());
+
+        var dotImage = new game.assets.Dot();
+        _dot = new Blob(dotImage);
+        _dotRadius = dotImage.width / 2;
+
         _dragged = null;
     }
 
     public function update(input : engine.Input) {
         if (input.mouse.pressed()) {
             if (_dragged == null) {
-                _dragged = findNearest(input.mouse.pos);
+                var nearest = findNearest(input.mouse.pos);
+                if (nearest != null && isInsideDot(input.mouse.pos, nearest)) {
+                    _dragged = nearest;
+                }
             }
 
             if (_dragged != null) {
@@ -51,19 +59,32 @@ class BoardState implements GameState {
         }
     }
 
+    /**
+     * Returns the nearest point or
+     * null if the list of points is empty.
+     */
     function findNearest(pos : Point) : Point {
         var minPoint : Point = null;
-        var minDiff : Float = 0;
+        var minDiff : Float = Math.POSITIVE_INFINITY;
         for (point in _points) {
-            var dx = (point.x - pos.x);
-            var dy = (point.y - pos.y);
-            var diff = dx*dx + dy*dy;
-            if (minPoint == null || diff < minDiff) {
+            var diff = sqDiff(pos, point);
+            if (diff < minDiff) {
                 minDiff = diff;
                 minPoint = point;
             }
         }
 
         return minPoint;
+    }
+
+    function sqDiff(a : Point, b : Point) {
+        var dx = (a.x - b.x);
+        var dy = (a.y - b.y);
+        return dx*dx + dy*dy;
+    }
+
+    function isInsideDot(pos : Point, point : Point) {
+        var d = Math.sqrt(sqDiff(pos, point));
+        return d < _dotRadius;
     }
 }
